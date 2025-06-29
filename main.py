32d7cef7de4b4f52e04c871a03b4738a4f2fe590
sergleq <ma4mal@gmail.com>
2025-06-29 16:28:44 +0300

    realaise move and rotate

--- main.py
+++ main.py
@@ -16,15 +16,18 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     while True:
+        dt = clock.tick() / 1000
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
+        player.update(dt)   
         screen.fill("black")
-        player.draw(screen)
+        player.draw(screen)   
         pygame.display.flip()
         clock.tick(60)
-        dt = clock.tick() / 1000
+        
         
 
 if __name__ == "__main__":
