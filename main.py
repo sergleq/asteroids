99b9360c082c4c5472910b3bf913c1275089d225
sergleq <ma4mal@gmail.com>
2025-06-29 15:56:27 +0300

    realaise triangle

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
 from constants import *
+from player import Player
 
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
+dt = 0
+
+player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 def main():
     print("Starting Asteroids!")
@@ -17,8 +21,11 @@ def main():
                 return
             
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
+        dt = clock.tick() / 1000
+        
 
 if __name__ == "__main__":
     main()
